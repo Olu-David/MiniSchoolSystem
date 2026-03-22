d0ca7ad9c0d919e12aeaa4edec4b56945666ef8b
STICKZZOFFICIAL <Olusanyadavid@yahoo.com>
2026-03-22 11:37:35 +0100

    ErroLoading

---
+++ MiniSchoolSystem/Controllers/AssignmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSchoolSystem.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+       
+        
+            TempData["info"] = "Quiz Coming Soon, Make Una calm down 😄";
+            return View();
+        }
+    }
+    }
+}
---
+++ MiniSchoolSystem/Controllers/QuizController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniSchoolSystem.Controllers
+{
+    public class QuizController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TempData["info"] = "Quiz Coming Soon, Make Una calm down 😄";
+            return View();
+        }
+    }
+}
