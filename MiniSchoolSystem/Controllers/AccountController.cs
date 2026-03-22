52c5aefb107cf35961847afbb45ed90ca0dba673
Olusanya David Victor <122713195+Olu-David@users.noreply.github.com>
2026-03-22 20:40:04 +0100

    Update AccountController.cs

--- MiniSchoolSystem/Controllers/AccountController.cs
+++ MiniSchoolSystem/Controllers/AccountController.cs
@@ -69,6 +69,7 @@ namespace MiniSchoolSystem.Controllers
                     return View(model);
                 }
             }
+            Temp["Error"]="Registration Successful";
 
             return RedirectToAction(nameof(Login));
 
